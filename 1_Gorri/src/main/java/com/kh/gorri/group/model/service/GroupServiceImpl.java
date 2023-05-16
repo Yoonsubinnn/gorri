ddded05d84a3e1a8f75ec12a3ff8826523f6b1e3
hj lee <127376553+srMan-ANMS@users.noreply.github.com>
2023-05-16 12:19:07 +0900

    그룹 오류 수정

--- 1_Gorri/src/main/java/com/kh/gorri/group/model/service/GroupServiceImpl.java
+++ 1_Gorri/src/main/java/com/kh/gorri/group/model/service/GroupServiceImpl.java
@@ -1,6 +1,7 @@
 package com.kh.gorri.group.model.service;
 
 import java.util.ArrayList;
+import java.util.Properties;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,10 @@ import org.springframework.stereotype.Service;
 
 import com.kh.gorri.common.model.vo.PageInfo;
 import com.kh.gorri.group.model.dao.GroupDAO;
+import com.kh.gorri.group.model.vo.Attachment;
 import com.kh.gorri.group.model.vo.Group;
 import com.kh.gorri.group.model.vo.GroupBoard;
+import com.kh.gorri.group.model.vo.GroupMember;
 
 @Service
 public class GroupServiceImpl implements GroupService {
@@ -21,21 +24,68 @@ public class GroupServiceImpl implements GroupService {
 	private GroupDAO gDAO;
 
 	@Override
+	public int insertGroup(Group g) {
+		return gDAO.insertGroup(sqlSession, g);
+	}
+
+	@Override
+	public int insertAttm(Attachment a) {
+		return gDAO.insertAttm(sqlSession, a);
+	}
+
+	@Override
+	public int insertGroupMember(GroupMember gm) {
+		return gDAO.insertGroupMember(sqlSession, gm);
+	}
+
+	@Override
+	public int getListCount() {
+		return gDAO.getListCount(sqlSession);
+	}
+
+	@Override
+	public ArrayList<Group> selectGroupList(PageInfo pi) {
+		return gDAO.selectGroupList(sqlSession, pi);
+	}
+
+	@Override
+	public ArrayList<Attachment> selectAttmGroupList() {
+		return gDAO.selectAttmGroupList(sqlSession);
+	}
+
+	@Override
+	public ArrayList<Group> searchGroup(Properties search) {
+		return gDAO.searchGroupList(sqlSession, search);
+	}
+
+
+	@Override
 	public ArrayList<Group> selectGroupView(int groupNo) {
 		return gDAO.selectGroupView(sqlSession, groupNo);
 	}
-	
+
 	@Override
 	public int getListCount(int i) {
 		return gDAO.selectListCount(sqlSession, i);
 	}
-	
+
 	@Override
 	public ArrayList<GroupBoard> selectBoardList(PageInfo pi, int i) {
 		return gDAO.selectBoardList(sqlSession, pi, i);
 	}
+
 	@Override
 	public GroupBoard selectDetailBoard(GroupBoard gb) {
 		return gDAO.selectDetailBoard(sqlSession, gb);
+	   }
+
+	@Override
+	public ArrayList<Group> selectGroupListWithHost(PageInfo pi) {
+		return gDAO.selectGroupListWithHost(sqlSession, pi);
 	}
+
+	
+	
+
+	
 }
--- 1_Gorri/src/main/java/com/kh/gorri/group/model/vo/Group.java
+++ 1_Gorri/src/main/java/com/kh/gorri/group/model/vo/Group.java
@@ -10,9 +10,8 @@ public class Group {
 	private String category;
 	private String hostId;
 	
-	
-	public Group() {};
-	
+	public Group() {}
+
 	public Group(int membershipNo, String membershipName, String membershipContent, int capacity,
 			String membershipStatus, String hostPhone, String category, String hostId) {
 		super();
@@ -26,91 +25,74 @@ public class Group {
 		this.hostId = hostId;
 	}
 
-	
 	public int getMembershipNo() {
 		return membershipNo;
 	}
-
 
 	public void setMembershipNo(int membershipNo) {
 		this.membershipNo = membershipNo;
 	}
 
-
 	public String getMembershipName() {
 		return membershipName;
 	}
-
 
 	public void setMembershipName(String membershipName) {
 		this.membershipName = membershipName;
 	}
 
-
 	public String getMembershipContent() {
 		return membershipContent;
 	}
-
 
 	public void setMembershipContent(String membershipContent) {
 		this.membershipContent = membershipContent;
 	}
 
-
 	public int getCapacity() {
 		return capacity;
 	}
-
 
 	public void setCapacity(int capacity) {
 		this.capacity = capacity;
 	}
 
-
 	public String getMembershipStatus() {
 		return membershipStatus;
 	}
 
-
-	public void setMembershipStatus(String membershipStatus) {
+	public void setMembership_status(String membershipStatus) {
 		this.membershipStatus = membershipStatus;
 	}
-
 
 	public String getHostPhone() {
 		return hostPhone;
 	}
 
-
 	public void setHostPhone(String hostPhone) {
 		this.hostPhone = hostPhone;
 	}
-
 
 	public String getCategory() {
 		return category;
 	}
 
-
 	public void setCategory(String category) {
 		this.category = category;
 	}
-
 
 	public String getHostId() {
 		return hostId;
 	}
 
-
 	public void setHostId(String hostId) {
 		this.hostId = hostId;
 	}
 
-
 	@Override
 	public String toString() {
 		return "Group [membershipNo=" + membershipNo + ", membershipName=" + membershipName + ", membershipContent="
-				+ membershipContent + ", capacity=" + capacity + ", membershipStatus=" + membershipStatus
+				+ membershipContent + ", capacity=" + capacity + ", membership_status=" + membershipStatus
 				+ ", hostPhone=" + hostPhone + ", category=" + category + ", hostId=" + hostId + "]";
 	}
 	
